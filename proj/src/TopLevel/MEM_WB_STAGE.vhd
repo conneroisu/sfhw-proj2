85aa7f749bd99ba7dd0c1d2a192e2c5b6c0a191b
aidanfoss <quantumaidan@gmail.com>
2024-11-07 09:57:02 -0600

    fixing extra control signals

--- proj/src/TopLevel/MEM_WB_STAGE.vhd
+++ proj/src/TopLevel/MEM_WB_STAGE.vhd
@@ -1,3 +1,5 @@
+--Aidan Foss
+--Stage 4
 library ieee;
 use ieee.std_logic_1164.all;
 use iee.numeric_std.all;
@@ -17,14 +19,16 @@ entity MEM_WB_STAGE is
                 i_mux       : in std_logic_vector((DATA_WIDTH -1) downto 0);
                 o_mux       : in std_logic_vector((DATA_WIDTH -1) downto 0);
                 
+                --control signals, commented out signals because they are listed as wb/mem but they are actually not needed.
                 i_regWrite  : in std_logic;
                 i_memToReg  : in std_logic;
-                i_memRead   : in std_logic;
-                i_memWrite  : in std_logic;
-                i_pcSRC     : in std_logic;
+                --i_memRead   : in std_logic;
+                --i_memWrite  : in std_logic;
+                --i_pcSRC     : in std_logic;
+                i_clk       : in std_logic;
                 o_regWrite  : in std_logic;
                 o_memToReg  : in std_logic;
-                o_memRead   : in std_logic;
-                o_memWrite  : in std_logic;
-                o_pcSRC     : in std_logic;
+                --o_memRead   : in std_logic;
+                --o_memWrite  : in std_logic;
+                --o_pcSRC     : in std_logic;
         )
