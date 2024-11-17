8bd132be400ca81a39c0b6acbb32d30387978e43
Conner Ohnesorge <conneroisu@outlook.com>
2024-11-16 20:33:03 -0600

    make sure passed in do is a full string cmd

--- proj/test/tb_muxNtM.sh
+++ proj/test/tb_muxNtM.sh
@@ -12,4 +12,4 @@ fi
 
 export PATH=$PATH:/usr/local/mentor/questasim/bin
 export SALT_LICENSE_SERVER=$SALT_LICENSE_SERVER:1717@io.ece.iastate.edu
-/usr/local/mentor/questasim/bin/vsim -do do ./tb_muxNtM.do
+/usr/local/mentor/questasim/bin/vsim -do 'do ./tb_muxNtM.do'
