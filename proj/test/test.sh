21157dfbd82cc50426a7414d613ba17f6ffe7c71
Conner Ohnesorge <conneroisu@outlook.com>
2024-11-16 20:36:05 -0600

    added scripts to run tests

--- proj/test/test.sh
+++
@@ -1,6 +0,0 @@
-
-parent_path=$( cd "$(dirname "$0")" ; pwd -P )
-echo "parent_path: $parent_path"
-# split to the last '/'
-last_slash=$(echo "$parent_path" | rev | cut -d'/' -f1 | rev)
-echo "last_slash: $last_slash"
