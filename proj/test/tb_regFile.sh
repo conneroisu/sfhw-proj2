83d916ba3e770c37a31a566b135c92622fce4e5b
Conner Ohnesorge <conneroisu@outlook.com>
2024-11-16 21:08:36 -0600

    update path given to vsim on all scripts

--- proj/test/tb_regFile.sh
+++ proj/test/tb_regFile.sh
@@ -13,4 +13,4 @@ fi
 
 export PATH=$PATH:/usr/local/mentor/questasim/bin
 export SALT_LICENSE_SERVER=$SALT_LICENSE_SERVER:1717@io.ece.iastate.edu
-/usr/local/mentor/questasim/bin/vsim -do "do ./$target.do"
+/usr/local/mentor/questasim/bin/vsim -do "do ./$target.do" .
