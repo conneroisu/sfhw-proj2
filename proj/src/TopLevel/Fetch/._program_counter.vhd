454085406c6c8a052336b2815763279483cd0857
Conner Ohnesorge <conneroisu@outlook.com>
2024-11-21 10:36:49 -0600

    remove ._ files

--- proj/src/TopLevel/Fetch/._program_counter.vhd
+++
@@ -1,9 +0,0 @@
--- <header>
--- Author(s): Kariniux
--- Name: proj/src/TopLevel/Fetch/._program_counter.vhd
--- Notes:
---      Kariniux 2024-11-21T09:04:48-06:00 pushing-pulling
--- </header>
-
-
-
