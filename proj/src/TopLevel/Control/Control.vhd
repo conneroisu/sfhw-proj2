a8fb80251aaa711a317a214dc44e186d31c5fa29
Conner Ohnesorge <conneroisu@outlook.com>
2024-11-29 15:49:48 -0600

    remove process from Control.vhd

--- proj/src/TopLevel/Control/Control.vhd
+++ proj/src/TopLevel/Control/Control.vhd
@@ -49,36 +49,5 @@ architecture structure of Control is
 begin
 
 
-    ControlProc : process(i_opcode, i_Rt, i_Rs, i_Rd, i_Shamt, i_Funct, i_Imm, o_PCWriteCond, o_PCWrite, o_IorD, o_MemtoReg, o_IRWrite, o_PCSrc, o_ALUOp, o_ALUSrcA, o_ALUSrcB, o_RegWrite, o_RegDst, o_Halt)
-    begin
-        case i_opcode is
-            --      R-format instructions (opcode = 000000)
-            -- |31    26|25  21|20  16|15  11|10    6|5     0|
-            -- |---------------------------------------------|
-            -- | opcode |  rs  |  rt  |  rd  | shamt | funct |
-            -- |---------------------------------------------|
-            -- |6 bits  |5 bits|5 bits|5 bits|5 bits |6 bits |
-            when "000000" =>
-                o_RegDst <= "01";
-
-            --      J-format instructions (opcode = 000010 or 000011)
-            -- |31    26|25                                 0|
-            -- |---------------------------------------------|
-            -- | opcode |         address                    |
-            -- |---------------------------------------------|
-            -- |6 bits  |        26 bits                     |
-            when "000010" | "000011" =>
-                o_RegDst <= "10";
-            --      I-format instructions (all other opcodes)
-            -- |31    26|25  21|20  16|15                   0|
-            -- |---------------------------------------------|
-            -- | opcode |  rs  |  rt  |       immediate      |
-            -- |---------------------------------------------|
-            -- |6 bits  |5 bits|5 bits|       16 bits        |
-            when others =>
-                o_RegDst <= "00";
-        end case;
-    end process;
-
 end structure;
 
