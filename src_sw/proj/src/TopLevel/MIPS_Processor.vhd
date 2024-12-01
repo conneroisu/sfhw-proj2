b77ed25c6e1e82fd6581f572de8e8482e16c8a4d
Conner Ohnesorge <conneroisu@outlook.com>
2024-12-01 17:41:03 -0600

    fix signal lenght for lui

--- src_sw/proj/src/TopLevel/MIPS_Processor.vhd
+++ src_sw/proj/src/TopLevel/MIPS_Processor.vhd
@@ -67,7 +67,7 @@ architecture structure of MIPS_Processor is
     signal s_DMemWr : std_logic;
     signal s_DMemWrAddr : std_logic_vector(N - 1 downto 0);
     signal s_EX_Inst_imm : std_logic_vector(15 downto 0);
-    signal s_EX_lui_val : std_logic_vector(15 downto 0);
+    signal s_EX_lui_val : std_logic_vector(32 downto 0);
     signal s_MEM_Inst_imm : std_logic_vector(15 downto 0);
     signal s_MEM_lui_val : std_logic_vector(15 downto 0);
     signal s_MEM_PCP4 : std_logic_vector(31 downto 0);
