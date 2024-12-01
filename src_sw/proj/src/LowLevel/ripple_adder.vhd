e78942d9496d6736f0d48241ca3d1bd86924bcce
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-12-01 12:05:06 -0600

    fix ripple adder and add tests

--- src_sw/proj/src/LowLevel/ripple_adder.vhd
+++ src_sw/proj/src/LowLevel/ripple_adder.vhd
@@ -2,7 +2,6 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity ripple_adder is
-  --Default to 32-bit wide
   generic (N : integer := 32);
   port (
         i_A     : in std_logic_vector(N-1 downto 0);
@@ -24,17 +23,17 @@ architecture structural of ripple_adder is
     );
   end component;
 
-  --N bits because we need to be able to assign the final carry out
+  -- N bits because we need to be able to assign the final carry out
   signal s_carry : std_logic_vector(N downto 0);
 begin
-  --First bit carry vector should be the initial carry in
-  --Last bit of carry vector should be the carry out
+  -- First bit carry vector should be the initial carry in
+  -- Last bit of carry vector is the final carry out
   s_carry(0) <= i_Cin;
   o_Cout <= s_carry(N-1);
 
   g_ripple_carry : for i in 0 to N-1 generate
-    --Set carry in to be first bit of s_carry which is set before generate loop
-    --Set the carry out to the next bit of the carry signal vector so it can be grabbed by next iteration of generate loop
+    -- Set carry in to be first bit of s_carry which is set before generate loop
+    -- Set carry out to carry signal vector's next bit for use in next full adder
     FULL_ADDER_N : full_adder port map (i_A(i), i_B(i), s_carry(i), o_Sum(i), s_carry(i+1));
   end generate g_ripple_carry;
 
