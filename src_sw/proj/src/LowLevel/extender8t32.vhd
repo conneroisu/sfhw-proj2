0aa203944af31844d77625b940f8d28a6065416a
Conner Ohnesorge <conneroisu@outlook.com>
2024-12-01 16:16:37 -0600

    make extender8t32 fit styleguide

--- src_sw/proj/src/LowLevel/extender8t32.vhd
+++ src_sw/proj/src/LowLevel/extender8t32.vhd
@@ -14,16 +14,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity extender8t32 is
+    
     port(
         i_I : in  std_logic_vector(7 downto 0);  -- byte value
         i_C : in  std_logic;                     -- signed extender or unsigned
         o_O : out std_logic_vector(31 downto 0)  -- 32 bit extended value
         );
+    
 end extender8t32;
 
 architecture dataflow of extender8t32 is
     signal ext_bit : std_logic;         -- sign extension bit
---signal extended : std_logic_vector(31 downto 0);  -- extended immediate
 begin
 
     o_O(7 downto 0) <= i_I(7 downto 0);  --copy bits we already have
