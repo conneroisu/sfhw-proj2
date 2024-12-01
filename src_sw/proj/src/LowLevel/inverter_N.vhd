58a0ae5b2d0d393ac7212fbfda334e451476e93b
Conner Ohnesorge <conneroisu@outlook.com>
2024-12-01 16:15:35 -0600

    make inverter_N fit styleguide

--- src_sw/proj/src/LowLevel/inverter_N.vhd
+++ src_sw/proj/src/LowLevel/inverter_N.vhd
@@ -2,17 +2,24 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity inverter_N is
-  generic (N : integer := 16);    --Input/output data width
+
+  generic (
+    N : integer := 16
+    );                                  --Input/output data width
+
   port (
-        i_Data    : in std_logic_vector(N-1 downto 0);
-        o_Data    : out std_logic_vector(N-1 downto 0)
-  );
+    i_Data : in  std_logic_vector(N-1 downto 0);
+    o_Data : out std_logic_vector(N-1 downto 0)
+    );
+
 end inverter_N;
 
 architecture structural of inverter_N is
   component invg is
-    port( i_A          : in std_logic;
-          o_F          : out std_logic);
+    port(
+      i_A : in  std_logic;
+      o_F : out std_logic
+      );
   end component;
 
 begin
