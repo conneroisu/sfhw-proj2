eb058acad82e9a8fccc5d8335e93630045658e92
Conner Ohnesorge <conneroisu@outlook.com>
2024-12-01 16:17:38 -0600

    make decoder_5t32 fit styleguide

--- src_sw/proj/src/LowLevel/decoder_5t32.vhd
+++ src_sw/proj/src/LowLevel/decoder_5t32.vhd
@@ -12,13 +12,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_arith.all;
+
 entity decoder5t32 is
+
     port
         (
             i_i : in  std_logic_vector(4 downto 0);  -- 5-bit input
             o_o : out std_logic_vector(31 downto 0)  -- 32-bit output
             );
+
 end entity decoder5t32;
+
 architecture mixed of decoder5t32 is
 
 begin
@@ -59,4 +63,3 @@ begin
         "00000000000000000000000000000000" when others;
 
 end architecture mixed;
-
