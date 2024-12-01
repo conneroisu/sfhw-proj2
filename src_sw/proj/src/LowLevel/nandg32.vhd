10dce2c5f6fa8d1b809a9b0cd9f1bb445601fa13
Conner Ohnesorge <conneroisu@outlook.com>
2024-12-01 16:12:02 -0600

    make nandg32 fit styleguide

--- src_sw/proj/src/LowLevel/nandg32.vhd
+++ src_sw/proj/src/LowLevel/nandg32.vhd
@@ -8,15 +8,20 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;
+
 entity nandg32 is
-    port(i_A : in  std_logic_vector(31 downto 0);
-         i_B : in  std_logic_vector(31 downto 0);
-         o_F : out std_logic_vector(31 downto 0));
+
+    port(
+        i_A : in  std_logic_vector(31 downto 0);
+        i_B : in  std_logic_vector(31 downto 0);
+        o_F : out std_logic_vector(31 downto 0)
+        );
+
 end nandg32;
+
 architecture dataflow of nandg32 is
 begin
     G1 : for i in 0 to 31 generate
         o_F(i) <= i_A(i) nand i_B(i);
     end generate;
 end dataflow;
-
