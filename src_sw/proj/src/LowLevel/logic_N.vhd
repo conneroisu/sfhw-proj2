4d94b145ac8b374557278d95ec553ac395f69fbd
Conner Ohnesorge <conneroisu@outlook.com>
2024-12-01 16:14:56 -0600

    make logic_N fit styleguide

--- src_sw/proj/src/LowLevel/logic_N.vhd
+++ src_sw/proj/src/LowLevel/logic_N.vhd
@@ -3,13 +3,17 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity logic_N is
-  generic (N : integer := 32);
+  generic (
+    N : integer := 32
+    );
+
   port (
-        i_D0      : in std_logic_vector(N-1 downto 0);
-        i_D1      : in std_logic_vector(N-1 downto 0);
-        i_op      : in std_logic_vector(1 downto 0);
-        o_result  : out std_logic_vector(N-1 downto 0)
-  );
+    i_D0     : in  std_logic_vector(N-1 downto 0);
+    i_D1     : in  std_logic_vector(N-1 downto 0);
+    i_op     : in  std_logic_vector(1 downto 0);
+    o_result : out std_logic_vector(N-1 downto 0)
+    );
+
 end logic_N;
 
 architecture dataflow of logic_N is
@@ -17,9 +21,9 @@ architecture dataflow of logic_N is
 begin
   s_others <= b"0";
   with i_op select
-    o_result <= i_D0 AND i_D1 when b"00",
-                i_D0 OR i_D1 when b"01",
-                i_D0 NOR i_D1 when b"10",
-                i_D0 XOR i_D1 when b"11",
-                std_logic_vector(resize(unsigned(s_others), o_result'length)) when others;
+    o_result <= i_D0 and i_D1                                     when b"00",
+    i_D0 or i_D1                                                  when b"01",
+    i_D0 nor i_D1                                                 when b"10",
+    i_D0 xor i_D1                                                 when b"11",
+    std_logic_vector(resize(unsigned(s_others), o_result'length)) when others;
 end dataflow;
