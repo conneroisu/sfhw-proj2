cc5ad0218925a0a3eac46518350c74902a005083
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-12-01 11:21:06 -0600

    update avliable ports in the processor's declaration of ID_EX

--- proj/src/TopLevel/MIPS_Processor.vhd
+++ proj/src/TopLevel/MIPS_Processor.vhd
@@ -82,10 +82,10 @@ architecture structure of MIPS_Processor is
 
     port (
         -- Common Stage Signals [begin]
-        i_CLK        : in  std_logic;
-        i_RST        : in  std_logic;
-        i_WE         : in  std_logic;
-        i_PC         : in  std_logic_vector(N-1 downto 0);
+        i_CLK       : in  std_logic;
+        i_RST       : in  std_logic;
+        i_WE        : in  std_logic;
+        i_PC        : in  std_logic_vector(N-1 downto 0);
         -- Control Signals (From Control Unit) [begin]
         --= Stage Specific Signals [begin]
         --         RegDst  ALUOp  ALUSrc
@@ -93,29 +93,29 @@ architecture structure of MIPS_Processor is
         -- lw    :   0      00      01
         -- sw    :   x      00      01
         -- beq   :   x      01      00
-        i_RegDst     : in  std_logic;   -- Control Unit Destination Register
-        i_ALUOp      : in  std_logic_vector(2 downto 0);  -- ALU operation from control unit.
-        i_ALUSrc     : in  std_logic_vector(1 downto 0);  -- ALU source from control unit.
-        i_MemRead    : in  std_logic;   -- Memory Read control
-        i_MemWrite   : in  std_logic;   -- Memory Write control
-        i_MemtoReg   : in  std_logic;   -- Memory to Register control
-        i_RegWrite   : in  std_logic;   -- Register Write control
-        i_Branch     : in  std_logic;   -- Branch control
+        i_RegDst    : in  std_logic;    -- Control Unit Destination Register
+        i_ALUOp     : in  std_logic_vector(2 downto 0);  -- ALU operation from control unit.
+        i_ALUSrc    : in  std_logic_vector(1 downto 0);  -- ALU source from control unit.
+        i_MemRead   : in  std_logic;    -- Memory Read control
+        i_MemWrite  : in  std_logic;    -- Memory Write control
+        i_MemtoReg  : in  std_logic;    -- Memory to Register control
+        i_RegWrite  : in  std_logic;    -- Register Write control
+        i_Branch    : in  std_logic;    -- Branch control
         -- Future Stage Signals [begin]
         -- see: https://private-user-images.githubusercontent.com/88785126/384028866-8e8d5e84-ca22-462e-8b85-ea1c00c43e8f.png
-        o_ALU        : out std_logic_vector(N-1 downto 0);
-        o_ALUSrc     : out std_logic_vector(1 downto 0);
-        o_MemRead    : out std_logic;
-        o_MemWrite   : out std_logic;
-        o_MemtoReg   : out std_logic;
-        o_RegWrite   : out std_logic;
-        o_Branch     : out std_logic;
+        o_ALU       : out std_logic_vector(N-1 downto 0);
+        o_ALUSrc    : out std_logic_vector(1 downto 0);
+        o_MemRead   : out std_logic;
+        o_MemWrite  : out std_logic;
+        o_MemtoReg  : out std_logic;
+        o_RegWrite  : out std_logic;
+        o_Branch    : out std_logic;
         -- Input Signals [begin]
         --= Register File Signals [begin]
-        i_Read1      : in  std_logic_vector(N-1 downto 0);
-        i_Read2      : in  std_logic_vector(N-1 downto 0);
-        o_Read1      : out std_logic_vector(N-1 downto 0);
-        o_Read2      : out std_logic_vector(N-1 downto 0);
+        i_Read1     : in  std_logic_vector(N-1 downto 0);
+        i_Read2     : in  std_logic_vector(N-1 downto 0);
+        o_Read1     : out std_logic_vector(N-1 downto 0);
+        o_Read2     : out std_logic_vector(N-1 downto 0);
         -- Forward Unit Signals [begin]
         --= Forwarded Signals (received from Forward Unit) [begin]
         -- ForwardA & ForwardB determine 1st & 2nd alu operands respectively
@@ -126,21 +126,23 @@ architecture structure of MIPS_Processor is
         -- ForwardB=00  -> ID/EX    -> operand from registerfile
         -- ForwardB=10  -> EX/MEM   -> operand forwarded from prior alu result
         -- ForwardB=01  -> MEM/WB   -> operand forwarded from dmem or earlier alu result
-        i_ForwardA   : in  std_logic_vector(1 downto 0);
-        i_ForwardB   : in  std_logic_vector(1 downto 0);
+        i_ForwardA  : in  std_logic_vector(1 downto 0);
+        i_ForwardB  : in  std_logic_vector(1 downto 0);
         --= Forwarding Signals (sent to Forward Unit) [begin]
-        i_WriteData  : in  std_logic_vector(N-1 downto 0);  -- Data from the end of writeback stage's mux
-        i_DMem1      : in  std_logic_vector(N-1 downto 0);  -- Data from the first input to the DMem output of ex/mem
+        i_WriteData : in  std_logic_vector(N-1 downto 0);  -- Data from the end of writeback stage's mux
+        i_DMem1     : in  std_logic_vector(N-1 downto 0);  -- Data from the first input to the DMem output of ex/mem
         --= Instruction Signals [begin]
-        i_Rs         : in  std_logic_vector(4 downto 0);
-        i_Rt         : in  std_logic_vector(4 downto 0);
-        i_Rd         : in  std_logic_vector(4 downto 0);
-        i_Shamt      : in  std_logic_vector(4 downto 0);
-        i_Funct      : in  std_logic_vector(5 downto 0);
-        i_Imm        : in  std_logic_vector(15 downto 0);
+        i_Rs        : in  std_logic_vector(4 downto 0);
+        i_Rt        : in  std_logic_vector(4 downto 0);
+        i_Rd        : in  std_logic_vector(4 downto 0);
+        i_Shamt     : in  std_logic_vector(4 downto 0);
+        i_Funct     : in  std_logic_vector(5 downto 0);
+        i_Imm       : in  std_logic_vector(15 downto 0);
+        i_Extended     : in  std_logic_vector(31 downto 0);
+        o_BranchAddr   : out std_logic_vector(31 downto 0);
         -- Halt signals
-        i_Halt       : in  std_logic_vector(0 downto 0);
-        o_Halt       : out std_logic_vector(0 downto 0)
+        i_Halt      : in  std_logic_vector(0 downto 0);
+        o_Halt      : out std_logic_vector(0 downto 0)
         );
 
     end component;
