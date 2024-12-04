bc568318030af8c41b3f6e2939c16458997a4d6f
Conner Ohnesorge <connero@iastate.edu>
2024-12-03 22:28:54 -0600

    better spacing in control unit and remove branch unitn

--- src_hw/proj/src/TopLevel/ControlUnit.vhd
+++ src_hw/proj/src/TopLevel/ControlUnit.vhd
@@ -33,8 +33,8 @@ architecture behavioral of ControlUnit is
 begin
     process(i_opcode, i_funct)
     begin
-        if i_opcode /= "000000" then        --I and J type instructions
-            if i_opCode = "001000" then     --addi 
+        if i_opcode /= "000000" then        -- I and J type instructions
+            if i_opCode = "001000" then     -- addi 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -54,7 +54,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "001001" then  --addiu 
+            elsif i_opCode = "001001" then  -- addiu 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -74,7 +74,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "001100" then  --andi 
+            elsif i_opCode = "001100" then  -- andi 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -94,7 +94,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "001111" then  --lui 
+            elsif i_opCode = "001111" then  -- lui 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -114,7 +114,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '1';
                 o_halt      <= '0';
-            elsif i_opCode = "100011" then  --lw 
+            elsif i_opCode = "100011" then  -- lw 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '1';
@@ -134,7 +134,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "001110" then  --xori 
+            elsif i_opCode = "001110" then  -- xori 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -154,7 +154,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "001101" then  --ori 
+            elsif i_opCode = "001101" then  -- ori 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -174,7 +174,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "001010" then  --slti 
+            elsif i_opCode = "001010" then  -- slti 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -194,7 +194,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "101011" then  --sw 
+            elsif i_opCode = "101011" then  -- sw 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
@@ -214,7 +214,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "000100" then  --beq 
+            elsif i_opCode = "000100" then  -- beq 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
@@ -234,7 +234,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "000101" then  --bne 
+            elsif i_opCode = "000101" then  -- bne 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
@@ -254,7 +254,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "000010" then  --j 
+            elsif i_opCode = "000010" then  -- j 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
@@ -274,7 +274,7 @@ begin
                 o_branch    <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "000011" then  --jal 
+            elsif i_opCode = "000011" then  -- jal 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -294,7 +294,7 @@ begin
                 o_branch    <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_opCode = "010100" then  --halt  
+            elsif i_opCode = "010100" then  -- halt  
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
@@ -336,7 +336,7 @@ begin
                 o_halt      <= '0';
             end if;
         else                                -- Handle R-Type instruciton
-            if i_funct = "100000" then      --add 
+            if i_funct = "100000" then      -- add 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -356,7 +356,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100001" then   --addu 
+            elsif i_funct = "100001" then   -- addu 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -376,7 +376,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100100" then   --and 
+            elsif i_funct = "100100" then   -- and 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -396,7 +396,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100111" then   --nor 
+            elsif i_funct = "100111" then   -- nor 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -416,7 +416,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100110" then   --xor 
+            elsif i_funct = "100110" then   -- xor 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -436,7 +436,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100101" then   --or 
+            elsif i_funct = "100101" then   -- or 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -456,7 +456,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "101010" then   --slt 
+            elsif i_funct = "101010" then   -- slt 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -476,7 +476,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "000000" then   --sll 
+            elsif i_funct = "000000" then   -- sll 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -496,7 +496,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "000010" then   --srl 
+            elsif i_funct = "000010" then   -- srl 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -516,7 +516,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "000011" then   --sra 
+            elsif i_funct = "000011" then   -- sra 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -536,7 +536,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100010" then   --sub 
+            elsif i_funct = "100010" then   -- sub 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -556,7 +556,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "100011" then   --subu 
+            elsif i_funct = "100011" then   -- subu 
                 o_RegDst    <= '1';
                 o_RegWrite  <= '1';
                 o_memToReg  <= '0';
@@ -576,7 +576,7 @@ begin
                 o_jump      <= '0';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            elsif i_funct = "001000" then   --jr 
+            elsif i_funct = "001000" then   -- jr 
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
@@ -596,7 +596,7 @@ begin
                 o_jump      <= '1';
                 o_lui       <= '0';
                 o_halt      <= '0';
-            else
+            else                            -- UNKNOWN
                 o_RegDst    <= '0';
                 o_RegWrite  <= '0';
                 o_memToReg  <= '0';
