0af6416c8b318b9d46a75cb833f70d504ed537cd
Conner Ohnesorge <connero@iastate.edu>
2024-12-04 05:21:59 -0600

    clean up adder subtractor unit

--- src_hw/proj/src/LowLevel/adderSubtractor.vhd
+++ src_hw/proj/src/LowLevel/adderSubtractor.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity adderSubtractor is
-    generic (N : integer := 32);  -- Using 16 because that is the max of the 2to1 MUX
+    generic (N : integer := 32);
 
     port (
         nAdd_Sub  : in  std_logic;
@@ -29,7 +29,7 @@ architecture structural of adderSubtractor is
     end component;
 
     component comp1_N is
-        generic (N : integer := 32);  -- Generic of type integer for input/output data width. Default value is 32.
+        generic (N : integer := 32);
         port (
             i_D0 : in  std_logic_vector(N - 1 downto 0);
             o_O  : out std_logic_vector(N - 1 downto 0)
@@ -37,7 +37,7 @@ architecture structural of adderSubtractor is
     end component;
 
     component mux2t1_N is
-        generic (N : integer := 32);  -- Generic of type integer for input/output data width. Default value is 32.
+        generic (N : integer := 32);
         port (
             i_S  : in  std_logic;
             i_D0 : in  std_logic_vector(N - 1 downto 0);
@@ -46,31 +46,35 @@ architecture structural of adderSubtractor is
             );
     end component;
 
-    signal n_B   : std_logic_vector(N - 1 downto 0);  --not B, used for subtraction
-    signal mux_B : std_logic_vector(N - 1 downto 0);  --B after the multiplexor
+    signal s_notB   : std_logic_vector(N - 1 downto 0);
+    signal s_muxedB : std_logic_vector(N - 1 downto 0);
 begin
 
-    OnesComp1 : comp1_N
+    instOnesComps : comp1_N
+        generic map(N => 32)
         port map(
             i_D0 => i_B,
-            o_O  => n_B
+            o_O  => s_notB
             );
 
-    MUX1 : mux2t1_N
+    instMux2t1s : mux2t1_N
+        generic map(N => 32)
         port map(
             i_S  => nAdd_Sub,
             i_D0 => i_B,
-            i_D1 => n_B,  --chooses regular b for a select of 0, and not b for a select of 1
-            o_O  => mux_B
+            i_D1 => s_notB,
+            o_O  => s_muxedB
             );
 
-    FullAdderN1 : Full_Adder_N
+    instFullAdders : Full_Adder_N
+        generic map(N => 32)
         port map(
             i_A        => i_A,
-            i_B        => mux_B,
+            i_B        => s_muxedB,
             i_C        => nAdd_Sub,
             o_S        => o_S,
             o_C        => o_C,
             o_Overflow => o_OF
             );
+
 end structural;
