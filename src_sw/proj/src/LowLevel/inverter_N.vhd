83e631723be02fd96c303b765c1207869815d7c1
connerohnesorge <conneroisu@outlook.com>
2024-12-04 21:41:14 -0600

    remove unnecesary comment on inverter_N

--- src_sw/proj/src/LowLevel/inverter_N.vhd
+++ src_sw/proj/src/LowLevel/inverter_N.vhd
@@ -9,7 +9,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity inverter_N is
-    generic (N : integer := 16);        --Input/output data width
+    generic (N : integer := 16);
 
     port (
         i_Data : in  std_logic_vector(N-1 downto 0);
@@ -27,7 +27,7 @@ architecture structural of inverter_N is
     end component;
 
 begin
-    
+
     g_nbit_inverter : for i in 0 to N-1 generate
         INVERTER_N : invg port map (i_Data(i), o_Data(i));
     end generate g_nbit_inverter;
