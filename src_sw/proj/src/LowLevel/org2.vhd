aa3b9fdfa2d20027620b4e74c68d89a8b26ce2f0
connerohnesorge <conneroisu@outlook.com>
2024-12-04 21:38:30 -0600

    make org2 fit styleguide

--- src_sw/proj/src/LowLevel/org2.vhd
+++ src_sw/proj/src/LowLevel/org2.vhd
@@ -8,13 +8,17 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+
 entity org2 is
+
     port (
         i_a : in  std_logic;
         i_b : in  std_logic;
         o_f : out std_logic
         );
+
 end entity org2;
+
 architecture dataflow of org2 is
 begin
     o_f <= i_a or i_b;
