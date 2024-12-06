71482a5c4ddb9c156c8fcbda1c3e5a28ac9ede47
aidanfoss <quantumaidan@gmail.com>
2024-12-06 15:14:06 -0600

    added stages

--- src_hw/proj/test/tb_Stages.vhd
+++ src_hw/proj/test/tb_Stages.vhd
@@ -26,7 +26,6 @@ architecture behavior of tb_stages is
     end component;
     
     component ID_EX is
-
         port (
                 i_CLK               : in  std_logic;
                 i_Reset             : in  std_logic;
@@ -116,7 +115,183 @@ architecture behavior of tb_stages is
             o_PC4      : out std_logic_vector(31 downto 0)
             );
     end component;
-    
+    signal 
+    IF_PC4, -- vec 32
+    IF_Instruction, -- vec 32
+    ID_PC4, -- vec 32
+    ID_Instruction,-- vec 32
+    ID_RegisterFileReadA,-- vec 32
+    ID_RegisterFileReadB,-- vec 32
+    ID_ImmediateExtended,-- vec 32
+    EX_RegisterFileReadA,--vec 32
+    EX_RegisterFileReadB,--vec 32
+    EX_ImmediateExtended,--vec 32
+    EX_PC4,--vec 32
+    EX_ALU, --vec 32
+    EX_B,--vec 32
+    MEM_ALU,--vec 32
+    MEM_B,--vec 32
+    MEM_PC4,--vec 32
+    WB_ALU,--vec 32
+    WB_Mem,--vec 32
+    WB_PC4,--vec 32
+    MEM_Mem--vec 32
+    :STD_LOGIC_VECTOR(31 downto 0);
+    signal
+    ID_IDRt, --vec 5
+    ID_IDRd, -- vec 5
+    ID_RS, --vec 5
+    EX_RS, -- vec 5
+    EX_Rt,--vec 5
+    EX_Rd,--vec 5
+    EX_WrAddr, --vec 5
+    MEM_WrAddr,--vec 5
+    WB_WrAddr:--vec 5
+    std_logic_vector(4 downto 0);
+    signal
+    ID_ALUOp, -- vec 4
+    EX_ALUOp: -- vec 4
+    STD_LOGIC_VECTOR(3 downto 0);
+    signal
+    IF_CLK,
+    IF_RST,
+    IF_Stall,
+    ID_CLK,
+    ID_Reset,
+    ID_Stall,
+    ID_RegDst,
+    ID_RegWrite,
+    ID_MemToReg,
+    ID_MemWrite,
+    ID_ALUSrc,
+    ID_Jal, 
+    ID_Halt,
+    ID_MEMRd,
+    EX_RegDst,
+    EX_RegWrite,
+    EX_MemWrite,
+    EX_ALUSrc,
+    EX_Halt,
+    EX_MEMRd,
+    EX_CLK,
+    EX_RST,
+    EX_stall,
+    EX_MemWr,
+    EX_MemtoReg,
+    EX_RegWr,
+    EX_Jal,
+    MEM_MemWr,
+    MEM_MemtoReg,
+    MEM_Halt,
+    MEM_RegWr,
+    MEM_Jal,
+    MEM_CLK,
+    MEM_RST,
+    MEM_stall,
+    WB_MemtoReg,
+    WB_Halt,
+    WB_RegWr,
+    WB_Jal
+    : std_logic;
 begin
+
+    IF_ID_inst: IF_ID
+    port map(
+       i_CLK => IF_CLK,
+       i_RST => IF_RST,
+       i_Stall => IF_Stall,
+       i_PC4 => IF_PC4,
+       i_Instruction => IF_Instruction,
+       o_PC4 => ID_PC4,
+       o_Instruction => ID_Instruction
+   );
+
+    ID_EX_inst: ID_EX
+     port map(
+        i_CLK =>        ID_CLK,
+        i_Reset =>      ID_Reset,
+        i_Stall =>      ID_Stall,
+        i_PC4 =>        ID_PC4,
+        i_RegisterFileReadA =>  ID_RegisterFileReadA,
+        i_RegisterFileReadB =>  ID_RegisterFileReadB,
+        i_ImmediateExtended =>  ID_ImmediateExtended,
+        i_IDRt =>               ID_IDRt,
+        i_IDRd =>               ID_IDRd,
+        i_RegDst =>             ID_RegDst,
+        i_RegWrite =>           ID_RegWrite,
+        i_MemToReg =>           ID_MemToReg,
+        i_MemWrite =>           ID_MemWrite,
+        i_ALUSrc =>             ID_ALUSrc,
+        i_ALUOp =>              ID_ALUOp,
+        i_Jal =>                ID_Jal,
+        i_Halt =>               ID_Halt,
+        i_RS =>                 ID_RS,
+        i_MEMRd =>              ID_MEMRd,
+        o_RS =>                 EX_RS,
+        o_PC4 =>                EX_PC4,
+        o_RegisterFileReadA =>  EX_RegisterFileReadA,
+        o_RegisterFileReadB =>  EX_RegisterFileReadB,
+        o_ImmediateExtended =>  EX_ImmediateExtended,
+        o_Rt =>                 EX_Rt,
+        o_Rd =>                 EX_Rd,
+        o_RegDst =>             EX_RegDst,
+        o_RegWrite =>           EX_RegWrite,
+        o_memToReg =>           EX_MemToReg,
+        o_MemWrite =>           EX_MemWrite,
+        o_ALUSrc =>             EX_ALUSrc,
+        o_ALUOp =>              EX_ALUOp,
+        o_Jal =>                EX_Jal,
+        o_Halt =>               EX_Halt,
+        o_MEMRd =>              EX_MEMRd
+    );
+
+    EX_MEM_inst: EX_MEM
+     port map(
+        i_CLK => EX_CLK,
+        i_RST => EX_RST,
+        i_stall => EX_stall,
+        i_ALU => EX_ALU,
+        i_B => EX_B,
+        i_WrAddr => EX_WrAddr,
+        i_MemWr => EX_MemWr,
+        i_MemtoReg => EX_MemtoReg,
+        i_Halt => EX_Halt,
+        i_RegWr => EX_RegWr,
+        i_Jal => EX_Jal,
+        i_PC4 => EX_PC4,
+        o_ALU => MEM_ALU,
+        o_B => MEM_B,
+        o_WrAddr => MEM_WrAddr,
+        o_MemWr => MEM_MemWr,
+        o_MemtoReg =>   MEM_MemtoReg,
+        o_Halt =>       MEM_Halt,
+        o_RegWr =>      MEM_RegWr,
+        o_Jal =>        MEM_Jal,
+        o_PC4 =>        MEM_PC4
+    );
+
+    MEM_WB_inst: MEM_WB
+     port map(
+        i_CLK =>        MEM_CLK,
+        i_RST =>        MEM_RST,
+        i_stall =>      MEM_stall,
+        i_ALU =>        MEM_ALU,
+        i_Mem =>        MEM_Mem,
+        i_WrAddr =>     MEM_WrAddr,
+        i_MemtoReg =>   MEM_MemtoReg,
+        i_Halt =>       MEM_Halt,
+        i_RegWr =>      MEM_RegWr,
+        i_Jal =>        MEM_Jal,
+        i_PC4 =>        MEM_PC4,
+        o_ALU =>        WB_ALU,
+        o_Mem =>        WB_Mem,
+        o_WrAddr =>     WB_WrAddr,
+        o_MemtoReg =>   WB_MemtoReg,
+        o_Halt =>       WB_Halt,
+        o_RegWr =>      WB_RegWr,
+        o_Jal =>        WB_Jal,
+        o_PC4 =>        WB_PC4
+    );
+
 
 end behavior;
