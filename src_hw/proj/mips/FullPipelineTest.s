4b5cbc97293003ed4fdce8cfbc07855d2a7c0dee
aidanfoss <quantumaidan@gmail.com>
2024-12-06 21:59:17 -0600

    fullPipeLineTest cant do structural hazards

--- src_hw/proj/mips/FullPipelineTest.s
+++ src_hw/proj/mips/FullPipelineTest.s
@@ -1,13 +1,24 @@
 .text
 main:
-add $t1, $t2, $t3
-sub $t4, $t1, $t5
-and $t6, $t4, $t7
-lw $t8, 0($t6)
-sw $t8, 4($t6)
-beq $t1, $t2, Label
-or $t9, $t6, $t8
-add $t1, $t9, $t3
+addi $t1, $zero, 5
+addi $t2, $zero, 10
+addi $t3, $zero, 15
+addi $t4, $zero, 20
+addi $t5, $zero, 25
+addi $t6, $zero, 30
+addi $t7, $zero, 35
+addi $t8, $zero, 40
+addi $t9, $zero, 45
+add $1, $2, $3
+sub $4, $1, $5
+and $6, $4, $7
+
+beq $1, $2, Label
+or $9, $6, $8
+add $1, $9, $3
 Label:
-xor $t4, $t1, $t7
-jr $ra
+xor $4, $1, $7
+
+halt
+lw $8, 0($6)
+sw $8, 4($6)
