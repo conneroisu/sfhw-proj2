259469c2f582af003919f7adb06cce674e1dc48b
Conner Ohnesorge <connerohnesorge@Conners-MacBook-Air.local>
2024-12-07 05:54:37 -0600

    ensured that andg32 fits the styleguide

--- src_hw/proj/src/LowLevel/andg32.vhd
+++ src_hw/proj/src/LowLevel/andg32.vhd
@@ -7,6 +7,7 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;
+
 entity andg32 is
     port
         (
@@ -15,6 +16,7 @@ entity andg32 is
             o_F : out std_logic_vector(31 downto 0)   -- output F
             );
 end andg32;
+
 architecture dataflow of andg32 is
 begin
     G1 : for i in 0 to 31 generate
