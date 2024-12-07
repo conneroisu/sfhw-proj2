d8cc7aa9c1df83fa9ab54f5f5d71f1e2a5e47aae
aidanfoss <quantumaidan@gmail.com>
2024-12-06 22:07:46 -0600

    struct haz

--- src_hw/proj/mips/CtrlHaz.s
+++ src_hw/proj/mips/CtrlHaz.s
@@ -5,4 +5,4 @@ add $3, $4, $5
 sub $6, $7, $8
 Label:
 or $9, $1, $2
-jr $ra
+halt
--- src_hw/proj/mips/StructHaz.s
+++ src_hw/proj/mips/StructHaz.s
@@ -1,6 +1,8 @@
+.data
+memory: .word 0x12345678, 0x9abcdef0, 0x11223344, 0x55667788
 .text
 main:
-    lw $1, 0($2)   
-    sw $1, 4($3)      
-    add $4, $1, $5  
-    halt
+lw $1, 0($2)   
+sw $1, 4($3)      
+add $4, $1, $5  
+halt
