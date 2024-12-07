679cbeb2dfef8d6cf134923af5933b538eb76abb
Aidan Foss <44009137+aidanfoss@users.noreply.github.com>
2024-12-06 22:24:10 -0600

    Update StructHaz.s

--- src_hw/proj/mips/StructHaz.s
+++ src_hw/proj/mips/StructHaz.s
@@ -3,12 +3,12 @@ array: .word 10, 20, 30, 40
 
 .text
 main:
-    addi $t0, $zero, 0       # Initialize base address index to 0
-    addi $t1, $zero, 100     # Initialize data value to 100
+    addi $0, $zero, 0       # Initialize base address index to 0
+    addi $1, $zero, 100     # Initialize data value to 100
 
-    lw $t2, 0($t0)           # Load value from array[0]
-    addi $t3, $t2, 5         # Perform an operation with the loaded value
-    sw $t1, 4($t0)           # Store 100 to array[1]
-    addi $t4, $t1, 10        # Another addi instruction around sw
+    lw $2, 0($0)           # Load value from array[0]
+    addi $3, $2, 5         # Perform an operation with the loaded value
+    sw $1, 4($0)           # Store 100 to array[1]
+    addi $4, $1, 10        # Another addi instruction around sw
 
     halt                     # End program
