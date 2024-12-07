f8cde17f800c6ba6e3417fe3f646bc1d31af8d05
Aidan Foss <44009137+aidanfoss@users.noreply.github.com>
2024-12-06 22:14:41 -0600

    Update StructHaz.s

--- src_hw/proj/mips/StructHaz.s
+++ src_hw/proj/mips/StructHaz.s
@@ -2,7 +2,7 @@
 memory: .word 0x12345678, 0x9abcdef0, 0x11223344, 0x55667788
 .text
 main:
-lw $1, 0($2)
-sw $1, 4($3)
-add $4, $1, $5
-halt
+    lw $t1, memory($t2)
+    sw $t1, 4($t3)
+    add $t4, $t1, $t5
+    halt
