d6d6e079d1a8904ba92b684a71dee99db50c822c
connerohnesorge <conneroisu@outlook.com>
2024-12-13 12:41:17 -0600

    format and add comments of data hazards to mips/countTo1000.s

--- mips/countTo1000.s
+++ mips/countTo1000.s
@@ -2,10 +2,10 @@
 .text
 .globl main
 main:
-addi $1, $1, 0
-addi $2, $1, 1000
+    addi $1, $1, 0
+    addi $2, $1, 1000 # data hazard
 
 loop1:
-addi $1, $1, 1
-bne $1, $2, loop1 
+    addi $1, $1, 1    # data hazard
+    bne $1, $2, loop1 # data hazard
 halt
